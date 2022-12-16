2711244a28aec238bb72f18f76654d536fb4dae0
selene jofre <mselenejofre@gmail.com>
2022-12-16 01:05:47 -0300

    eliminando archivos

--- script.js
+++
@@ -1,5 +0,0 @@
-const switchButton = document.getElementById("switch");
-switchButton.addEventListener("click",()=>{
-    document.body.classList.toggle("dark");
-    switchButton.classList.toggle("active")
-})
